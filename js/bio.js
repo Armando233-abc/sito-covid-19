0cadd584176a2cc670bd3f533e920680e4b49664
Armando233-abc <armandovigliotti5@gmail.com>
2020-11-25 11:55:20 +0100

    aggiunta funzioni bottoni biografia

---
+++ js/bio.js
@@ -0,0 +1,9 @@
+function indietro(){
+    testo = document.querySelector("#testo")
+    testo.innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit numquam dicta laborum, fugiat nihil quae repellendus animi architecto autem, quas nostrum natus distinctio. Distinctio voluptatum blanditiis, earum ut pariatur illum?"
+}
+
+function avanti(){
+    testo = document.querySelector("#testo")
+    testo.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, officia, sint suscipit autem ea libero aut excepturi quo rem, vel explicabo facilis nisi quas eligendi assumenda veniam ipsa vero asperiores!"
+}
