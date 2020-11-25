142b3fa88a4dab97a3e7d07b224d096d2985fcdd
Armando233-abc <armandovigliotti5@gmail.com>
2020-11-25 12:29:48 +0100

    aggiunta immagini

--- js/bio.js
+++ js/bio.js
@@ -1,9 +1,13 @@
-function indietro(){
-    testo = document.querySelector("#testo")
+function avanti(){
+    let testo = document.querySelector("#testo")
+    let img = document.querySelector("#img_bio")
     testo.innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit numquam dicta laborum, fugiat nihil quae repellendus animi architecto autem, quas nostrum natus distinctio. Distinctio voluptatum blanditiis, earum ut pariatur illum?"
+    img.src = "/images/armando.png"
 }
 
-function avanti(){
-    testo = document.querySelector("#testo")
+function indietro(){
+    let testo = document.querySelector("#testo")
+    let img = document.querySelector("#img_bio")
     testo.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, officia, sint suscipit autem ea libero aut excepturi quo rem, vel explicabo facilis nisi quas eligendi assumenda veniam ipsa vero asperiores!"
+    img.src = "/images/antonio.png"
 }
